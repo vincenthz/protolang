26c756aeb2d42130cca6076d6535d16d16440a15
Vincent Hanquez <vincent@coretype.io>
2024-01-10 19:30:36 +0800

    tweak sources

--- protolang/src/lib.rs
+++ protolang/src/lib.rs
@@ -3,7 +3,7 @@ mod environ;
 mod value;
 
 use std::{error::Error, fs, path::Path};
-use werbolg_core::{Literal, Module, Namespace};
+use werbolg_core::{Ident, Literal, Module, Namespace};
 use werbolg_lang_common::FileUnit;
 use werbolg_lang_rusty as rusty;
 
@@ -19,20 +19,39 @@ pub struct ProtocolState {}
 
 pub struct Actor {}
 
-pub fn sources<S: AsRef<Path>>(dir: S) -> Result<Vec<Module>, Box<dyn std::error::Error>> {
+pub fn source<S: AsRef<Path>>(path: S) -> Result<Module, Box<dyn std::error::Error>> {
+    let content = std::fs::read_to_string(&path.as_ref()).expect("file read");
+    let file_unit = FileUnit::from_string(path.as_ref().display().to_string(), content);
+
+    let module = rusty::module(&file_unit).map_err(|e| format!("{:?}", e))?;
+    Ok(module)
+}
+
+/// Load all the modules in the given path
+pub fn sources<S: AsRef<Path>>(
+    dir: S,
+) -> Result<Vec<(Namespace, Module)>, Box<dyn std::error::Error>> {
     let mut modules = Vec::new();
+    let base = dir.as_ref().to_path_buf();
 
-    for entry in fs::read_dir(dir).unwrap() {
-        let entry = entry.unwrap();
+    let dir_namespace = Namespace::root();
 
-        let entry_type = entry.file_type().unwrap();
-        if entry_type.is_file() {
-            let unit = get_file(entry.path()).unwrap();
-            let module = rusty::module(&unit).expect("Parse file");
-            modules.push(module);
-        } else if entry_type.is_dir() {
-            let path = entry.path();
-            panic!("Directory not supported yet: {}", path.display())
+    for file in fs::read_dir(dir)? {
+        let dir_ent = file?;
+        let dir_ent_type = dir_ent.file_type()?;
+        if dir_ent_type.is_file() {
+            let mut base = base.clone();
+            base.push(dir_ent.file_name());
+            let filename = dir_ent.file_name();
+            let s = filename.to_str().ok_or("")?;
+            if let Some(module_name) = s.strip_suffix(".protolang") {
+                let i = Ident::from(module_name);
+                let module_namespace = dir_namespace.clone().append(i);
+                let m = source(base)?;
+                modules.push((module_namespace, m))
+            }
+        } else if dir_ent_type.is_dir() {
+            // todo recurse
         }
     }
     Ok(modules)
@@ -64,13 +83,4 @@ pub fn compile(
         .map_err(|e| format!("compilation error {:?}", e))?;
 
     Ok(exec_module)
-}
-
-fn get_file<P: AsRef<Path>>(path: P) -> std::io::Result<FileUnit> {
-    let path = path.as_ref();
-    let filename = path.file_name().expect("File Name");
-
-    let content = std::fs::read_to_string(path).expect("file read");
-    let fileunit = FileUnit::from_string(filename.to_string_lossy().to_string(), content);
-    Ok(fileunit)
 }
